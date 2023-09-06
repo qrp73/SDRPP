05efd969e4ac14cf33693c2e7164798f25ffdd7c
qrp73 <46676744+qrp73@users.noreply.github.com>
2023-09-06 11:01:47 +0300

    fix hamming window coefficients

--- core/src/dsp/window/hamming.h
+++ core/src/dsp/window/hamming.h
@@ -3,7 +3,7 @@
 
 namespace dsp::window {
     inline double hamming(double n, double N) {
-        const double coefs[] = { 0.54, 0.46 };
+        const double coefs[] = { 0.53836, 0.46164 };
         return cosine(n, N, coefs, sizeof(coefs) / sizeof(double));
     }
 }
