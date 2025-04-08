233f8fb763a7fa0eda901644c64bd86e9dd5a4b1
qrp73 <46676744+qrp73@users.noreply.github.com>
2025-04-08 11:45:06 +0300

    fix imgui line width rendering issue

--- core/backends/sdl2/backend.cpp
+++ core/backends/sdl2/backend.cpp
@@ -165,6 +165,9 @@ namespace backend {
         // Setup Dear ImGui style
         ImGui::StyleColorsDark();
         //ImGui::StyleColorsLight();
+        
+        ImGui::GetStyle().AntiAliasedLines = false;
+        
 
         // Setup Platform/Renderer backends
         if (!ImGui_ImplSDL2_InitForOpenGL(window, gl_context))
