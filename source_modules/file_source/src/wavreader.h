8266b0072da68a6d3a4ce07439f1d009df526c8f
qrp73 <46676744+qrp73@users.noreply.github.com>
2025-04-15 14:14:27 +0300

    file_source: add support for WAVE_FORMAT_EXTENSIBLE (pcm/float, mono/stereo)

--- source_modules/file_source/src/wavreader.h
+++ source_modules/file_source/src/wavreader.h
@@ -106,19 +106,49 @@ public:
             memcpy(sbuf, &chunkId, 4);
             //flog::debug("chunk \"{0}\", size {1}", sbuf, chunkSize);
             if (memcmp(&chunkId, "fmt ", 4) == 0) {
-                _hdr.fmt_id = chunkId;
-                _hdr.fmt_size = chunkSize;
+                const auto fmt_Size = chunkSize;
+                if (fmt_Size < 16 || (fmt_Size > 16 && fmt_Size < 18)) {
+                    throw std::runtime_error("Invalid fmt chunk size " + std::to_string(fmt_Size));
+                }                
                 _file.read((char*)&_hdr.wFormatTag,       sizeof(_hdr.wFormatTag));
                 _file.read((char*)&_hdr.wChannels,        sizeof(_hdr.wChannels));
                 _file.read((char*)&_hdr.dwSamplesPerSec,  sizeof(_hdr.dwSamplesPerSec));
                 _file.read((char*)&_hdr.dwAvgBytesPerSec, sizeof(_hdr.dwAvgBytesPerSec));
                 _file.read((char*)&_hdr.wBlockAlign,      sizeof(_hdr.wBlockAlign));
                 _file.read((char*)&_hdr.wBitsPerSample,   sizeof(_hdr.wBitsPerSample));
-                if (chunkSize == 18)
-                {
-                    // read any extra values
-                    _file.read((char*)&_hdr.__zero,   sizeof(_hdr.__zero));
-                    _file.seekg(_hdr.__zero, std::ios_base::cur);
+                if (fmt_Size > 16) {
+                    uint16_t fmt_ExtraSize; // cbSize
+                    _file.read((char*)&fmt_ExtraSize, sizeof(fmt_ExtraSize));
+                    if (_hdr.wFormatTag == WAVE_FORMAT::EXTENSIBLE && fmt_ExtraSize >= 22) { // WAVE_FORMAT_EXTENSIBLE
+                        uint16_t validBitsPerSample;
+                        uint32_t channelMask;
+                        uint32_t subFormat_Data1;
+                        uint16_t subFormat_Data2;
+                        uint16_t subFormat_Data3;
+                        uint64_t subFormat_Data4;
+                        _file.read((char*)&validBitsPerSample, sizeof(validBitsPerSample));
+                        _file.read((char*)&channelMask, sizeof(channelMask));
+                        _file.read((char*)&subFormat_Data1, sizeof(subFormat_Data1));
+                        _file.read((char*)&subFormat_Data2, sizeof(subFormat_Data2));
+                        _file.read((char*)&subFormat_Data3, sizeof(subFormat_Data3));
+                        _file.read((char*)&subFormat_Data4, sizeof(subFormat_Data4));
+                        if (subFormat_Data1 == 1) {
+                            _hdr.wFormatTag = WAVE_FORMAT::PCM;
+                        } else if (subFormat_Data1 == 2) {
+                            _hdr.wFormatTag = WAVE_FORMAT::IEEE_FLOAT;
+                        } else {
+                            flog::warn("validBitsPerSample = {0}", validBitsPerSample);
+                            flog::warn("channelMask        = {0}", channelMask);
+                            flog::warn("subFormat.Data1    = {0}", subFormat_Data1);
+                            flog::warn("subFormat.Data2    = {0}", subFormat_Data2);
+                            flog::warn("subFormat.Data3    = {0}", subFormat_Data3);
+                            flog::warn("subFormat.Data4    = {0}", subFormat_Data4);
+                            throw std::runtime_error("Unknown format type for WAVE_FORMAT_EXTENSIBLE");
+                        }
+                    } else {
+                        // Skip extra bytes
+                        _file.seekg(fmt_ExtraSize, std::ios_base::cur);
+                    }
                 }
                 _fmtPresent = true;
             } else if (memcmp(&chunkId, "data", 4) == 0) {
@@ -166,16 +196,12 @@ public:
 
 private:
     struct RIFF_HDR_t {
-        // Format chunk (24 bytes)
-        uint32_t fmt_id;             // "fmt "
-        uint32_t fmt_size;//16?
         uint16_t wFormatTag;         // Format category
         uint16_t wChannels;          // Number of channels
         uint32_t dwSamplesPerSec;    // Sampling rate
         uint32_t dwAvgBytesPerSec;   // For buffer estimation
         uint16_t wBlockAlign;        // Data block size
         uint16_t wBitsPerSample;     // Sample size
-        uint16_t __zero;
 
         // Data chunk
         uint32_t data_id;            // "data"
