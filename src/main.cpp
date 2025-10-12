1fb2c3de7c97afe8f1ba856ced120fee7abd474e
qrp73 <46676744+qrp73@users.noreply.github.com>
2025-10-12 12:06:17 +0300

    add exception logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <core.h>
 #include <stdio.h>
+#include <utils/flog.h>
 
 int main(int argc, char* argv[]) {
-    return sdrpp_main(argc, argv);
+    try {
+        return sdrpp_main(argc, argv);
+    } catch (const std::exception& e) {
+        flog::exception(e);
+    } catch (...) {
+        flog::exception();
+    }
 }
